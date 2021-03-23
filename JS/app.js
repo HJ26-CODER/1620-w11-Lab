941f64b803407725cb24d6669047108630d03bf3
Harsh <hjscrt@gmail.com>
2021-03-23 16:07:09 -0700

    Finished week 11 lab

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,31 @@
-// your JavaScript goes here, you can delete this comment
+const todoList = [
+    {
+        todo: 'clone repo for starter code',
+        status: 'complete'
+    },
+    {
+        todo: 'link js and styles files to index.html',
+        status: 'complete'
+    },
+    {
+        todo: 'add js',
+        status: 'started'
+    },
+    {
+        todo: 'Finish 1620 with good grades',
+        status: 'started'
+    }
+];
+
+const completedTodos = [];
+
+for (const todo of todoList) {
+    if (todo.status == 'complete') {
+        completedTodos.push(todo)
+    }
+}
+
+for (const todoObj of completedTodos) {
+    ({ todo } = todoObj)
+    console.log(todo)
+}
